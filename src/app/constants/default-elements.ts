15b08ddba205c4ec73b5910cd7b2dd61693be41d
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-10-22 16:26:50 +0530

    default elements

--- src/app/constants/default-elements.ts
+++ src/app/constants/default-elements.ts
@@ -1,1 +1,22 @@
-exo
+export const defaultElement = [
+    {
+        text: "Fire",
+        emoji: "🔥",
+        discovered: false,
+    },
+    {
+        text: "Water",
+        emoji: "💧",
+        discovered: false,
+    },
+    {
+        text: "Wind",
+        emoji: "💨",
+        discovered: false,
+    },
+    {
+        text: "Earth",
+        emoji: "🌍",
+        discovered: false,
+    },
+];
